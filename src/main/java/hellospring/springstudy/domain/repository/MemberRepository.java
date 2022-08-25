2621212cf460934f4a2861c4864ab103f3df51b9
sungho95 <tjdgh1785@gmail.com>
2022-08-25 23:32:17 +0900

    2022-08-25

---
+++ src/main/java/hellospring/springstudy/domain/repository/MemberRepository.java
@@ -0,0 +1,18 @@
+package hellospring.springstudy.domain.repository;
+
+import hellospring.springstudy.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member); // 저장소에 저장
+    // 저장소에 저장된 Id나 Name을 findById, findByName으로 찾아올 수 있음
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(Long name);
+    // findAll을 하면 저장된 모든 정보를 리스트로 받아옴
+    List<Member> findAll();
+}
+
+// 리포지토리 작성 후 구현체를 만들어야 한다.
+
---
+++ src/main/java/hellospring/springstudy/domain/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package hellospring.springstudy.domain.repository;
+
+import hellospring.springstudy.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // 다음과 같이 할 경우 동시성 문제를 고려해야 하나, 간단하게 예제를 진행
+   private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(Long name) {
+        store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
