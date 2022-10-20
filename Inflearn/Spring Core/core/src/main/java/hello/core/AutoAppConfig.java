d59c5de69d9b309f41c6b1965562b94b421a39d3
Sungho95 <tjdgh1785@gmail.com>
2022-10-20 23:24:50 +0900

    2022-10-19 컴포넌트 스캔

---
+++ Inflearn/Spring Core/core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,24 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        //AppConfig.java 파일의 Configuration을 제외하기 위한 설정
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        // 탐색 위치 지정(Default: ComponentScan이 위치한 패키지부터)
+//        basePackages = {"hello.core.member", "hello.core.service"},
+//        basePackageClasses = AutoAppConfig.class
+)
+public class AutoAppConfig {
+    // 수동 등록 vs 자동 등록
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+}
