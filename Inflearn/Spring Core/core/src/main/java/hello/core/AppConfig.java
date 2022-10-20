572abc72bca5b60e2ef484009e27525a6e7975d0
Sungho95 <tjdgh1785@gmail.com>
2022-10-20 21:56:34 +0900

    2022-10-19 @Configuration & 싱글톤

--- Inflearn/Spring Core/core/src/main/java/hello/core/AppConfig.java
+++ Inflearn/Spring Core/core/src/main/java/hello/core/AppConfig.java
@@ -15,18 +15,26 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // memberRepository가 3번 호출될 것 같으나 1번밖에 호출되지 않음
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
