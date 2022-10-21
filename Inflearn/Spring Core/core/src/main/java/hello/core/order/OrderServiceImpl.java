7d1346aa0ea55fcb433c208cf041e95631373a33
Sungho95 <tjdgh1785@gmail.com>
2022-10-21 22:56:51 +0900

    2022-10-21 다양한 의존 관계 주입

--- Inflearn/Spring Core/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Inflearn/Spring Core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -13,7 +13,25 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
+//    // 필드 주입 (가급적 사용하지 않는 것이 좋음 - 안티패턴)
+//    @Autowired private MemberRepository memberRepository;
+//    @Autowired private DiscountPolicy discountPolicy;
+
+//    // 수정자 주입 (선택적이거나 변경 가능할 때 사용)
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
+//
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+//    @Autowired // 생성자가 딱 1개만 있으면, @Autowired를 생략할 수 있다.
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
