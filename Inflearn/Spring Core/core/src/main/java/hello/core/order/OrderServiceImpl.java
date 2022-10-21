a93de79bd6104986e084c2b622ea38440ccdded3
Sungho95 <tjdgh1785@gmail.com>
2022-10-21 22:57:12 +0900

    2022-10-21 다양한 의존 관계 주입

--- Inflearn/Spring Core/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Inflearn/Spring Core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,8 +3,6 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
