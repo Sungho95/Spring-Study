3fcfa902ac23df19ca1348ea74b5ddf99517c52a
Sungho95 <tjdgh1785@gmail.com>
2022-10-28 21:12:41 +0900

    2022-10-28 매핑

--- Inflearn/Spring MVC/springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ Inflearn/Spring MVC/springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -4,12 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController("/mapping/users")
 public class MappingController {
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping(value = "/hello-basic", method = RequestMethod.GET)
+    @GetMapping
     public String helloBasic() {
         log.info("helloBasic");
 
@@ -17,32 +17,32 @@ public class MappingController {
 
     }
 
-    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
-    public String mappingGetV1() {
-        log.info("mappingGetV1");
-        return "ok";
-    }
-
-    /**
-     * 편리한 축약 애노테이션 (코드보기)
-     * @GetMapping
-     * @PostMapping
-     * @PutMapping
-     * @DeleteMapping
-     * @PatchMapping
-     */
-    @GetMapping(value = "/mapping-get-v2")
-    public String mappingGetV2() {
-        log.info("mapping-get-v2");
-        return "ok";
-    }
+//    @GetMapping
+//    public String mappingGetV1() {
+//        log.info("mappingGetV1");
+//        return "ok";
+//    }
+//
+//    /**
+//     * 편리한 축약 애노테이션 (코드보기)
+//     * @GetMapping
+//     * @PostMapping
+//     * @PutMapping
+//     * @DeleteMapping
+//     * @PatchMapping
+//     */
+//    @GetMapping
+//    public String mappingGetV2() {
+//        log.info("mapping-get-v2");
+//        return "ok";
+//    }
 
     /**
      * PathVariable 사용
      * 변수명이 같으면 생략 가능
      * @PathVariable("userId") String userId -> @PathVariable userId
      */
-    @GetMapping("/mapping/{userId}")
+    @GetMapping("{userId}")
     public String mappingPath(@PathVariable("userId") String id) {
         log.info("mappingPath userId={}", id);
 
@@ -59,7 +59,7 @@ public class MappingController {
     /**
      * PathVariable 사용 다중
      */
-    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    @GetMapping("/{userId}/orders/{orderId}")
     public String mappingPath(@PathVariable String userId, @PathVariable Long
             orderId) {
         log.info("mappingPath userId={}, orderId={}", userId, orderId);
