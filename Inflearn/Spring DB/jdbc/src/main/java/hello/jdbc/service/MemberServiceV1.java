a72155ee22fc1a9bc3c29ad5fe3bee31dfdf00c7
Sungho95 <tjdgh1785@gmail.com>
2022-11-10 01:58:56 +0900

    2022-11-10 트랜잭션 적용2

--- Inflearn/Spring DB/jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
+++ Inflearn/Spring DB/jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -5,19 +5,23 @@ import hello.jdbc.repository.MemberRepositoryV1;
 import lombok.RequiredArgsConstructor;
 
 import java.sql.SQLException;
-
+// 트랜잭션은 어떤 계층? 어디에 시작하고 어디에 끝나야하는가?가 중요함.
+// 트랜잭션은 커넥션이 필요하며, 트랜잭션을 사용하는 동안 같은 커넥션을 유지해야함.
 @RequiredArgsConstructor
 public class MemberServiceV1 {
 
     private final MemberRepositoryV1 memberRepository;
 
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        // 트랜잭션 시작
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
 
         memberRepository.update(fromId, fromMember.getMoney() - money);
         validation(toMember);
         memberRepository.update(toId, toMember.getMoney() + money);
+        // 커밋
     }
 
     private static void validation(Member toMember) {
