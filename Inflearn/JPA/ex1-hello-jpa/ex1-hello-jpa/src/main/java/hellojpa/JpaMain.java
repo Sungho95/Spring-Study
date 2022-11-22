65499c957d2078b0e47e77d80179b2f01ea64fec
Sungho95 <tjdgh1785@gmail.com>
2022-11-22 22:24:14 +0900

    2022-11-22 - 지연 로딩과 즉시 로딩

--- Inflearn/JPA/ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Inflearn/JPA/ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -19,20 +19,27 @@ public class JpaMain {
             team.setName("teamA");
             em.persist(team);
 
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
+
             Member member1 = new Member();
             member1.setName("member1");
             member1.setTeam(team);
             em.persist(member1);
 
+            Member member2 = new Member();
+            member2.setName("member2");
+            member2.setTeam(teamB);
+            em.persist(member2);
+
             em.flush();
             em.clear();
 
-            Member m = em.find(Member.class, member1.getId());
-            System.out.println("m.getTeam().getClass() = " + m.getTeam().getClass());
+//            Member m = em.find(Member.class, member1.getId());
 
-            System.out.println("=========");
-            m.getTeam().getName();
-            System.out.println("=========");
+            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class)
+                    .getResultList();
 
             tx.commit();
         } catch (Exception e) {
