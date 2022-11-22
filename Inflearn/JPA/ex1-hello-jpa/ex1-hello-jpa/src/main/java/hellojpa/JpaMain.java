7d21d98b2293d761d2dacdd85c5121c323ca0637
Sungho95 <tjdgh1785@gmail.com>
2022-11-22 23:30:40 +0900

    2022-11-22 - 고아 객체

--- Inflearn/JPA/ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Inflearn/JPA/ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -25,6 +25,12 @@ public class JpaMain {
 
             em.persist(parent);
 
+            em.flush();
+            em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            em.remove(findParent);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
