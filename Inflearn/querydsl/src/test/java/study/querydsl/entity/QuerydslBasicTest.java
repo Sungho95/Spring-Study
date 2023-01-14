7f4247d1b2ead61bfb02d110401500e6f509aa40
Sungho95 <tjdgh1785@gmail.com>
2023-01-14 22:24:55 +0900

    querydsl - 상수, 문자 더하기

--- Inflearn/querydsl/src/test/java/study/querydsl/entity/QuerydslBasicTest.java
+++ Inflearn/querydsl/src/test/java/study/querydsl/entity/QuerydslBasicTest.java
@@ -2,7 +2,9 @@ package study.querydsl.entity;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -558,5 +560,36 @@ public class QuerydslBasicTest {
         }
     }
 
+    /**
+     * 상수
+     */
+    @Test
+    public void constant() throws Exception {
+        List<Tuple> result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
 
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * concat
+     */
+    @Test
+    public void concat() throws Exception {
+
+        // {username}_{age}
+        List<String> result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
 }
