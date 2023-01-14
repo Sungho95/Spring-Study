85579bfb863a7a78c95f8ba4209463aae98311da
Sungho95 <tjdgh1785@gmail.com>
2023-01-15 01:35:08 +0900

    querydsl - SQL function 호출하기

--- Inflearn/querydsl/src/test/java/study/querydsl/entity/QuerydslHighTest.java
+++ Inflearn/querydsl/src/test/java/study/querydsl/entity/QuerydslHighTest.java
@@ -6,6 +6,7 @@ import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
@@ -371,10 +372,41 @@ public class QuerydslHighTest {
 
     @Test
     public void bulkDelete() throws Exception {
-        queryFactory
+
+        long count = queryFactory
                 .delete(member)
                 .where(member.age.gt(18))
                 .execute();
     }
 
+    @Test
+    public void sqlFunction() throws Exception {
+
+        List<String> result = queryFactory
+                .select(Expressions.stringTemplate(
+                        "function('replace', {0}, {1}, {2})",
+                        member.username, "member", "M"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void sqlFunction2() throws Exception {
+
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+//                .where(member.username.eq(
+//                        Expressions.stringTemplate("function('lower', {0})", member.username)))
+                .where(member.username.eq(member.username.lower()))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
 }
