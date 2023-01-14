36a20365f503c2d7af857adcc8ba5e05f2f417a3
Sungho95 <tjdgh1785@gmail.com>
2023-01-14 22:10:08 +0900

    querydsl - 서브쿼리

--- Inflearn/querydsl/src/test/java/study/querydsl/entity/QuerydslBasicTest.java
+++ Inflearn/querydsl/src/test/java/study/querydsl/entity/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package study.querydsl.entity;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,6 +17,7 @@ import javax.persistence.PersistenceUnit;
 
 import java.util.List;
 
+import static com.querydsl.jpa.JPAExpressions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
@@ -349,14 +351,11 @@ public class QuerydslBasicTest {
             System.out.println("tuple = " + tuple);
         }
     }
-
     /**
      * 조인 대상을 필터링 해서 가져와야 하는 경우
      *  - leftJoin을 사용할 경우, on 절을 통해 조인 대상을 필터링 해야 한다.
      *  - innerJoin을 사용할 경우, where 절에서 필터링 할 수 있으며, on 절보다 깔끔하게 사용할 수 있다.
      */
-
-
 
     /**
      * 연관관계가 없는 엔티티의 외부 조인(세타 조인)
@@ -420,5 +419,109 @@ public class QuerydslBasicTest {
         assertThat(loaded).as("페치 조인 적용").isTrue();
 
     }
+
+    /**
+     * 서브 쿼리
+     * 나이가 가장 많은 회원 조회
+     */
+    @Test
+    public void subQuery() throws Exception {
+
+        QMember memberSub = new QMember("memberSub");
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(
+                        select(memberSub.age.max())
+                                .from(memberSub)
+                ))
+                .fetch();
+
+        assertThat(result).extracting("age")
+                .containsExactly(40);
+        assertThat(result.get(0).getAge()).isEqualTo(40);
+    }
+
+    /**
+     * 나이가 평균 이상인 회원 조회
+     * member.age.in(10, 20)        // age in (10,20)
+     * member.age.notIn(10, 20)     // age not in (10, 20)
+     * member.age.between(10,30)    // between 10, 30
+     * member.age.goe(30)           // age >= 30
+     * member.age.gt(30)            // age > 30
+     * member.age.loe(30)           // age <= 30
+     * member.age.lt(30)            // age < 30
+     */
+    @Test
+    public void subQueryGoe() throws Exception {
+
+        QMember memberSub = new QMember("memberSub");
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.goe(
+                        select(memberSub.age.avg())
+                                .from(memberSub)
+                ))
+                .fetch();
+
+        assertThat(result).extracting("age")
+                .containsExactly(30, 40);
+
+    }
+
+    /**
+     * 나이가 10보다 큰 회원 조회
+     */
+    @Test
+    public void subQueryIn() throws Exception {
+
+        QMember memberSub = new QMember("memberSub");
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.in(
+                        select(memberSub.age)
+                                .from(memberSub)
+                                .where(memberSub.age.gt(10))
+                ))
+                .fetch();
+
+        assertThat(result).extracting("age")
+                .containsExactly(20, 30, 40);
+
+    }
+
+    /**
+     * select 절 서브쿼리
+     */
+    @Test
+    public void selectSubQuery() throws Exception {
+
+        QMember memberSub = new QMember("memberSub");
+
+        List<Tuple> result = queryFactory
+                .select(member.username,
+                        select(memberSub.age.avg())
+                                .from(memberSub))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * 서브쿼리 정리
+     * from 절에서는 서브쿼리를 지원하지 않음.
+     * select 절은 하이버네이트 구현체를 사용하는 경우에 지원
+     */
+    /**
+     * form 절 서브쿼리 해결 방안
+     * - 서브쿼리를 joiin으로 변경 (불가능한 상황도 존재)
+     * - 애플리케이션에서 쿼리를 분리해서 실행
+     * - nativeSQL을 사용
+     */
 
 }
